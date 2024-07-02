e73b4e8a6893bb02ed80a2f5bd12d6730f418b67
rashaaz <rashaabdelaziz111@gmail.com>
2024-07-02 02:01:59 -0700

    add t0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+    Basic Flask app setup
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        Render the home page with a welcome message.
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
