a06c8fcf29ed12c53e2c40c29e727d90a12c878f
rashaaz <rashaabdelaziz111@gmail.com>
2024-07-02 02:15:54 -0700

    add t4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+'''
+    Flask app with Babel and locale selection
+'''
+
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    '''
+        Configuration for Flask app with Babel.
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def helloWorld() -> str:
+    '''
+        Render the home page with a welcome message.
+    '''
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''
+        Determine the best match for supported languages.
+    '''
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
